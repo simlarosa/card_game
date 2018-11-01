aec09e3dba0ccc1aabd7c300ef1c519661b74574
Simone La Rosa <SimoneMac@MacBook-Pro-di-Simone-2.local>
2018-11-01 02:42:03 +0100

    Add the react front end

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// React core that drives the Card Game Front End
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
